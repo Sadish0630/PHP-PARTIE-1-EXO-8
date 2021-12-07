c4ab4d94de88e4d9d72c77b103f64b8d0119615f
audemar <audemarsadish@gmail.com>
2021-12-07 15:27:23 +0100

    exo8 terminé

---
+++ exo8.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css"/>
+    <title>PARTIE 1 EXO 8</title>
+</head>
+<body>
+<p>
+   <?php
+    $a = '3 + 4';
+   echo $a = (3+4) ;
+   ?>
+   </p>
+   <p>
+   <?php
+   $b = '5 * 20';
+   echo $b = (5*20);
+   ?>
+   </p>
+   <p>
+   <?php
+   $c = '45 / 5';
+   echo $c = (45/5);
+   ?>
+   </p>
+
+</body>
+</html>
